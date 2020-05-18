8d6af00cf99dfba28f5d467db5ca6a1da57d8a2b
iMac <jonkerit@163.com>
2020-05-18 10:58:47 +0800

    修改脚本

--- podpack.sh
+++ podpack.sh
@@ -5,7 +5,7 @@ pod package BJLiveUI.podspec \
     --embedded \
     --no-mangle \
     --exclude-deps \
-    --spec-sources=http://git.baijiashilian.com/open-ios/BJLiveUI.git,https://github.com/CocoaPods/Specs.git \
+    --spec-sources=https://git.baijiashilian.com/open-ios/specs.git,https://github.com/CocoaPods/Specs.git \
     --verbose $args
 
 echo "完成"
